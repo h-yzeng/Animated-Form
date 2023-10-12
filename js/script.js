9f4af1908c0c2fcee2f5c920a148b4a981c64ac3
hzeng9 <hzeng9@hawk.iit.edu>
2023-10-12 17:18:56 -0500

    added addition html input attributes and added 2 js functions for input and completion

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+function formFocus(){
+  userForm.style.background = "AliceBlue"
+}   
+
+function formCompletion(){
+  let name = document.forms["userForm"]["name"].value;
+  let email = document.forms["userForm"]["email"].value;
+  let phone = document.forms["userForm"]["phone"].value;
+  if (name == "" || email == "" || phone == "") {
+    alert("Please fill out each field correctly.");
+    return false;
+  }else{
+    alert("Thank you for completing the form! We will reply back to you within 3-5 business days!");
+    return true;
+  } 
+} 
+
+
 
